2ed8cdf93e92d2e6890507e8bf091f22dfc583d0
YanethM <yaneth.mejiar@autonoma.edu.co>
2017-03-10 12:43:52 -0500

    Cambios en el diseño menu bootstrap

--- 03.Fuentes/resources/views/Prueba.blade.php
+++ 03.Fuentes/resources/views/Prueba.blade.php
@@ -1,16 +1,219 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" id ="">
+    <head>
+
+    <style type="text/css">
+    .nav-side-menu {
+  overflow: auto;
+  font-family: verdana;
+  font-size: 12px;
+  font-weight: 200;
+  background-color: #2e353d;
+  position: fixed;
+  top: 0px;
+  width: 300px;
+  height: 100%;
+  color: #e1ffff;
+}
+.nav-side-menu .brand {
+  background-color: #23282e;
+  line-height: 50px;
+  display: block;
+  text-align: center;
+  font-size: 14px;
+}
+.nav-side-menu .toggle-btn {
+  display: none;
+}
+.nav-side-menu ul,
+.nav-side-menu li {
+  list-style: none;
+  padding: 0px;
+  margin: 0px;
+  line-height: 35px;
+  cursor: pointer;
+
+    .collapsed{
+       .arrow:before{
+                 font-family: FontAwesome;
+                 content: "\f053";
+                 display: inline-block;
+                 padding-left:10px;
+                 padding-right: 10px;
+                 vertical-align: middle;
+                 float:right;
+            }
+     }
+
+}
+.nav-side-menu ul :not(collapsed) .arrow:before,
+.nav-side-menu li :not(collapsed) .arrow:before {
+  font-family: FontAwesome;
+  content: "\f078";
+  display: inline-block;
+  padding-left: 10px;
+  padding-right: 10px;
+  vertical-align: middle;
+  float: right;
+}
+.nav-side-menu ul .active,
+.nav-side-menu li .active {
+  border-left: 3px solid #d19b3d;
+  background-color: #4f5b69;
+}
+.nav-side-menu ul .sub-menu li.active,
+.nav-side-menu li .sub-menu li.active {
+  color: #d19b3d;
+}
+.nav-side-menu ul .sub-menu li.active a,
+.nav-side-menu li .sub-menu li.active a {
+  color: #d19b3d;
+}
+.nav-side-menu ul .sub-menu li,
+.nav-side-menu li .sub-menu li {
+  background-color: #181c20;
+  border: none;
+  line-height: 28px;
+  border-bottom: 1px solid #23282e;
+  margin-left: 0px;
+}
+.nav-side-menu ul .sub-menu li:hover,
+.nav-side-menu li .sub-menu li:hover {
+  background-color: #020203;
+}
+.nav-side-menu ul .sub-menu li:before,
+.nav-side-menu li .sub-menu li:before {
+  font-family: FontAwesome;
+  content: "\f105";
+  display: inline-block;
+  padding-left: 10px;
+  padding-right: 10px;
+  vertical-align: middle;
+}
+.nav-side-menu li {
+  padding-left: 0px;
+  border-left: 3px solid #2e353d;
+  border-bottom: 1px solid #23282e;
+}
+.nav-side-menu li a {
+  text-decoration: none;
+  color: #e1ffff;
+}
+.nav-side-menu li a i {
+  padding-left: 10px;
+  width: 20px;
+  padding-right: 20px;
+}
+.nav-side-menu li:hover {
+  border-left: 3px solid #d19b3d;
+  background-color: #4f5b69;
+  -webkit-transition: all 1s ease;
+  -moz-transition: all 1s ease;
+  -o-transition: all 1s ease;
+  -ms-transition: all 1s ease;
+  transition: all 1s ease;
+}
+@media (max-width: 767px) {
+  .nav-side-menu {
+    position: relative;
+    width: 100%;
+    margin-bottom: 10px;
+  }
+  .nav-side-menu .toggle-btn {
+    display: block;
+    cursor: pointer;
+    position: absolute;
+    right: 10px;
+    top: 10px;
+    z-index: 10 !important;
+    padding: 3px;
+    background-color: #ffffff;
+    color: #000;
+    width: 40px;
+    text-align: center;
+  }
+  .brand {
+    text-align: left !important;
+    font-size: 22px;
+    padding-left: 20px;
+    line-height: 50px !important;
+  }
+}
+@media (min-width: 767px) {
+  .nav-side-menu .menu-list .menu-content {
+    display: block;
+  }
+}
+body {
+  margin: 0px;
+  padding: 0px;
+}
+        </style>
+    </head>
+    <body>
 
 
-@section('content_title')
-Home
-@stop
+<div class="nav-side-menu">
+    <div class="brand">Parqueadero Rayo-carro</div>
+    <i class="fa fa-bars fa-2x toggle-btn" data-toggle="collapse" data-target="#menu-content"></i>
 
-@section('content')
-<p><span style="font-weight: bold; color: red;">
-<?php echo "Hola a todos"; ?></style>.
-</p>
-@stop
+        <div class="menu-list">
 
-@section('msg_success')
-Esta quedando muy bien
-@stop
+            <ul id="menu-content" class="menu-content collapse out">
+                <li>
+                  <a href="#">
+                  <i class="fa fa-dashboard fa-lg"></i> Usuarios
+                  </a>
+                </li>
+
+                <li  data-toggle="collapse" data-target="#products" class="collapsed active">
+                  <a href="#"><i class="fa fa-clock-o"></i>Entrada/Salida <span class="arrow"></span></a>
+                </li>
+                <ul class="sub-menu collapse" id="products">
+                    <li><a href="\RegistroES\new">Nuevo</a></li>
+                                                    <li><a href="\RegistroES\search">Consultar</a></li>
+                                                    <li><a href="\RegistroES\del">Eliminar</a></li>
+                </ul>
+
+
+
+
+
+                <li data-toggle="collapse" data-target="#new" class="collapsed">
+                  <a href="#"><i class="fa fa-car fa-lg"></i> Vehiculos <span class="arrow"></span></a>
+                </li>
+                <ul class="sub-menu collapse" id="new">
+                          <li><a href="\RegistroV\save">Nuevo</a></li>
+                                                    <li><a href="\RegistroV\find">Consultar</a></li>
+                                                    <li><a href="\RegistroV\search">Modificar</a></li>
+                                                    <li><a href="\RegistroV\delete">Eliminar</a></li>
+                </ul>
+
+
+               <li data-toggle="collapse" data-target="#new" class="collapsed">
+                  <a href="#"><i class="fa fa-dashboard fa-lg"></i> Conductores <span class="arrow"></span></a>
+                </li>
+                <ul class="sub-menu collapse" id="new">
+                   <li><a href="\RegistroC\save">Nuevo</a></li>
+                                                    <li><a href="\RegistroC\find">Consultar</a></li>
+                                                    <li><a href="\RegistroC\search">Modificar</a></li>
+                                                    <li><a href="\RegistroC\delete">Eliminar</a></li>
+                </ul>
+
+
+
+
+                       <li data-toggle="collapse" data-target="#new" class="collapsed">
+                  <a href="#"><i class="fa fa-user fa-lg"></i> Usuarios <span class="arrow"></span></a>
+                </li>
+                <ul class="sub-menu collapse" id="new">
+                     <li><a href="\RegistroU\save">Nuevo</a></li>
+                                                    <li><a href="\RegistroU\find">Consultar</a></li>
+                                                    <li><a href="\RegistroU\search">Modificar</a></li>
+                                                    <li><a href="\RegistroU\delete">Eliminar</a></li>
+                </ul>
+            </ul>
+     </div>
+</div>
+        </body>
--- 03.Fuentes/resources/views/layouts/app.blade.php
+++ 03.Fuentes/resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
      <title>Parqueadero Rayo - Carro</title>
 
     <link href="{{asset('css/bootstrap.css') }}" rel="stylesheet">
+    <link href="{{asset('css/estilo.css') }}" rel="stylesheet">
+        <link href="{{asset('css/font-awesome.css') }}" rel="stylesheet">
 
     </head>
     <body>
@@ -18,69 +20,152 @@
             </div>
         </div>
 
+
+
+
         <div class="container">
-            <div class="row">
-               <div class="col-md-3">
-                    <div class="panel panel-default">
-                        <div class="panel-body">
-                            <nav class="navbar navbar-default">
-                                <div class="container-fluid">
-                                    <!-- Brand and toggle get grouped for better mobile display -->
-                                    <div class="navbar-header">
-                                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                                            <span class="sr-only">Toggle navigation</span>
-                                            <span class="icon-bar"></span>
-                                            <span class="icon-bar"></span>
-                                            <span class="icon-bar"></span>
-                                        </button>
-
-                                    </div>
-
-                                    <!-- Collect the nav links, forms, and other content for toggling -->
-                                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                                        <ul class="nav navbar-nav">
-                                            <li class="dropdown">
-                                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Usuarios<span class="caret"></span></a>
-                                                <ul class="dropdown-menu">
-                                                    <li><a href="\RegistroU\save">Nuevo</a></li>
-                                                    <li><a href="\RegistroU\find">Consultar</a></li>
-                                                    <li><a href="\RegistroU\search">Modificar</a></li>
-                                                    <li><a href="\RegistroU\delete">Eliminar</a></li>
-                                                </ul>
-                                            </li>
-                                            <li class="dropdown">
-                                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Vehiculos<span class="caret"></span></a>
-                                                <ul class="dropdown-menu">
-                                                    <li><a href="\RegistroV\save">Nuevo</a></li>
-                                                    <li><a href="\RegistroV\find">Consultar</a></li>
-                                                    <li><a href="\RegistroV\search">Modificar</a></li>
-                                                    <li><a href="\RegistroV\delete">Eliminar</a></li>
-                                                </ul>
-                                            </li>
-                                            <li class="dropdown">
-                                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Conductores<span class="caret"></span></a>
-                                                <ul class="dropdown-menu">
-                                                    <li><a href="\RegistroC\save">Nuevo</a></li>
-                                                    <li><a href="\RegistroC\find">Consultar</a></li>
-                                                    <li><a href="\RegistroC\search">Modificar</a></li>
-                                                    <li><a href="\RegistroC\delete">Eliminar</a></li>
-                                                </ul>
-                                            </li>
-                                            <li class="dropdown">
-                                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Registro E/S<span class="caret"></span></a>
-                                                <ul class="dropdown-menu">
-                                                    <li><a href="\RegistroES\new">Nuevo</a></li>
-                                                    <li><a href="\RegistroES\search">Consultar</a></li>
-                                                    <li><a href="\RegistroES\del">Eliminar</a></li>
-                                                </ul>
-                                            </li>
-                                        </ul>
-                                    </div><!-- /.navbar-collapse -->
-                                </div><!-- /.container-fluid -->
-                            </nav>
+    <div class="row">
+        <div class="col-sm-3 col-md-3">
+            <div class="panel-group" id="accordion">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4 class="exte panel-title">
+                            <a id="unique" data-toggle="collapse" data-parent="#accordion" href="#collapseOne"><i class="fa fa-user-circle"></i>Usuarios</a>
+                        </h4>
+                    </div>
+                    <div id="collapseOne" class="panel-collapse collapse in">
+                        <div class="panel2 panel-body">
+                            <table class="table">
+                                <tr>
+                                    <td>
+                                        <i class="fa fa-plus-circle"></i><a href="\RegistroU\save">Nuevo</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <i class="fa fa-search-plus"></i><a href="\RegistroU\find">Consultar</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <i class="fa fa-pencil"></i><a href="\RegistroU\search">Modificar</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <i class="fa fa-eraser"></i><a href="\RegistroU\delete">Eliminar</a>
+                                    </td>
+                                </tr>
+                            </table>
                         </div>
                     </div>
                 </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo"><i class="fa fa-car"></i>Vehiculos</a>
+                        </h4>
+                    </div>
+                    <div id="collapseTwo" class="panel-collapse collapse">
+                        <div class="panel-body">
+                           <table class="table">
+                                <tr>
+                                    <td>
+                                        <i class="fa fa-plus-circle"></i><a href="\RegistroV\save">Nuevo</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <i class="fa fa-search-plus"></i><a href="\RegistroV\find">Consultar</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <i class="fa fa-pencil"></i><a href="\RegistroV\search">Modificar</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <i class="fa fa-eraser"></i><a href="\RegistroV\delete">Eliminar</a>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree"><i class="fa fa-user-o"></i>Conductores</a>
+                        </h4>
+                    </div>
+                    <div id="collapseThree" class="panel-collapse collapse">
+                        <div class="panel-body">
+                            <table class="table">
+                                <tr>
+                                    <td>
+                                        <i class="fa fa-plus-circle"></i><a href="\RegistroC\save">Nuevo</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <i class="fa fa-search-plus"></i><a href="\RegistroC\find">Consultar</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <i class="fa fa-pencil"></i><a href="\RegistroC\search">Modificar</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <i class="fa fa-eraser"></i><a href="\RegistroC\delete">Eliminar</a>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseFour"><i class="fa fa-clock-o"></i>Registro E/S</a>
+                        </h4>
+                    </div>
+                    <div id="collapseFour" class="panel-collapse collapse">
+                        <div class="panel-body">
+                           <table class="table">
+                                <tr>
+                                    <td>
+                                        <i class="fa fa-plus-circle"></i><a href="\RegistroES\new">Nuevo</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <i class="fa fa-search-plus"></i><a href="\RegistroES\search">Consultar</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <i class="fa fa-pencil"></i><a href="\RegistroES\find">Modificar</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <i class="fa fa-eraser"></i><a href="\RegistroES\find">Eliminar</a>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+  <script src="js/jquery-3.1.1.slim.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+
                 <div class="col-md-9">
                     <div class="panel panel-default">
                         <div class="panel-heading">
@@ -104,3 +189,7 @@
      {!! Html::script('assets/js/bootstrap.min.js') !!}
     </body>
 </html>
+
+
+
+
