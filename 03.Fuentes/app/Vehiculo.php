18ff2330fd430e0bcdb933ec30c7721e7dd2f81b
YanethM <yaneth.mejiar@autonoma.edu.co>
2017-03-14 11:20:02 -0500

    Modelo y entidad Vehiculos

---
+++ 03.Fuentes/app/Vehiculo.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Vehiculo extends Model
+{
+    //
+     protected $fillable = [
+        'name', 'email', 'password',
+    ];
+}
